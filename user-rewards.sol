6b478c3e422e551a4afeaa8728b08dea7f6b9b12
ajay-joshua <99689610+ajayjoshua-viral@users.noreply.github.com>
2022-02-24 23:29:36 +0530

    claim function added and bugs fixed

--- user-rewards.sol
+++ user-rewards.sol
@@ -7,15 +7,12 @@ import "@openzeppelin/contracts@4.5.0/access/Ownable.sol";
 import "@openzeppelin/contracts@4.5.0/security/Pausable.sol";
 
 contract Reward is ERC20, ERC20Snapshot, Ownable, Pausable {
+   
     uint256 public rewardamount;
     uint256 public rewardfortheuser;
     uint256 public impression = 0.0;
-    address[] public users = [0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db,0x78731D3Ca6b7E34aC0F824c42a7cC18A495cabaB,0x617F2E2fD72FD9D5503197092aC168c91465E7f2,0x17F6AD8Ef982297579C203069C1DbfFE4348c372];
-    struct Userinfo{
-        address payable userwallet;
-        uint256 userimpression;
-    }
-    mapping(address => Userinfo) userinfo1;
+    uint256 public totalimp = 0;
+
     constructor() ERC20("reward", "RWD") {}
 
     function snapshot() public onlyOwner {
@@ -44,13 +41,7 @@ contract Reward is ERC20, ERC20Snapshot, Ownable, Pausable {
     function totalrewardamount() external view onlyOwner returns(uint256){     // read 
          return rewardamount;
     }
-    function calculateimpressionpercent(uint256 impressionoftheuser, uint256 totalimpression)public onlyOwner{    //write
-       impression = ((impressionoftheuser*100) / totalimpression);
-    }
-    function sendreward(address payable user) public onlyOwner payable{         //write
-        rewardfortheuser = (rewardamount * impression)/100 ;
-        user.transfer(rewardfortheuser);
-    }
+
     function finalimpression() external view onlyOwner returns(uint256){         //read
         return impression;
     }
@@ -58,12 +49,15 @@ contract Reward is ERC20, ERC20Snapshot, Ownable, Pausable {
         return rewardfortheuser;
     }
 
-    function intialize() public view onlyOwner {
-        for(uint256 i = 0 ; i < users.length ; i++){
-           Userinfo memory userinfo = userinfo1[users[i]];
-           userinfo.userwallet = payable(users[i]);
-           userinfo.userimpression = 10;
-        }
+    function totalimpressionoftheday(uint256 totalImpressionOfTheDay) public onlyOwner{  //write
+          totalimp = totalImpressionOfTheDay;
+    }
+    
+    function claim(uint256 impressionoftheuser) public {                         //claim function
+        require(totalimp != 0,"you cant claim your rewards yet");
+        impression = ((impressionoftheuser*100) / totalimp);
+        rewardfortheuser = (rewardamount * impression)/100 ;
+        payable(msg.sender).transfer(rewardfortheuser);
     }
 
 }
